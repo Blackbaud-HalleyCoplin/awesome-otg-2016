cc6e6a2f2767e0167b9cfec3073a7047291ca4bb
Blackbaud-HalleyCoplin <halley.coplin@blackbaud.com>
2016-03-10 22:53:32 -0500

    Constituent details

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,4 +1,4 @@
-InboxSDK.load('1', 'Hello World!').then(function(sdk){
+InboxSDK.load('1.0', 'sdk_Sky-Integration_809ded04d4').then(function(sdk){
     var threadSidebars = new WeakMap();
     var threadLoadedConstituents = new WeakMap();
     
@@ -29,6 +29,34 @@ InboxSDK.load('1', 'Hello World!').then(function(sdk){
         );
     }
     
+    function populateConstituentSidebar(constituent, threadView)
+    {
+        showSidebar(threadView, {
+            name: constituent.name,
+            constituentRecordUrl: constituent.constituentRecordUrl,
+            phoneNumber: constituent.phoneNumber,
+            website: constituent.website,
+            spouseName: constituent.spouseName,
+            spouseRecordUrl: constituent.spouseRecordUrl,
+            constituentCodes: constituent.constituentCodes,
+            lastAction: {
+                type: "Meeting - Face to Face",
+                date: "1/2/2015",
+                summary: "Summary of action"
+            },
+            nextAction: {
+                type: "Meeting - Face to Face",
+                date: "6/2/2015",
+                summary: "Summary of action"
+            },
+            lastNote: {
+                type: "Biographical",
+                date: "3/7/2016",
+                summary: "Test Note"
+            }
+        });
+    }
+    
     sdk.Conversations.registerMessageViewHandler(function(messageView) {
         var possibleConstituents = messageView.getRecipients();
         possibleConstituents.push(messageView.getSender());
@@ -41,6 +69,8 @@ InboxSDK.load('1', 'Hello World!').then(function(sdk){
         
         var loadedConstituents = threadLoadedConstituents.get(threadView);
         var usersEmail = sdk.User.getEmailAddress();
+        var matchedConstituent = {};
+        var foundMatch = false;
         
         for (var i = 0;i<possibleConstituents.length;i++) {
             if ($.inArray(possibleConstituents[i].emailAddress, loadedConstituents) === -1 && usersEmail !== possibleConstituents[i].emailAddress) {
@@ -54,34 +84,71 @@ InboxSDK.load('1', 'Hello World!').then(function(sdk){
                     $.each(data.results, function (index, searchResult) {
                         if (searchResult.email === possibleConstituents[i].emailAddress) {
                             console.log(searchResult.id);
+                            matchedConstituent.id = searchResult.id;
+                            foundMatch = true;
                         }
                     });
                 });
                 
-                showSidebar(threadView, {
-                    name: possibleConstituents[i].name,
-                    constituentRecordUrl: "http://rexdev.blackbaud.com/constituents/280",
-                    phoneNumber: "843-234-1232",
-                    website: "http://blackbaud.com",
-                    spouseName: "Wendy Hernandez",
-                    spouseRecordUrl: "http://rexdev.blackbaud.com/constituents/410",
-                    constituentCodes: "Major Donor Prospect, Board Chair",
-                    lastAction: {
-                        type: "Meeting - Face to Face",
-                        date: "1/2/2015",
-                        summary: "Summary of action"
-                    },
-                    nextAction: {
-                        type: "Meeting - Face to Face",
-                        date: "6/2/2015",
-                        summary: "Summary of action"
-                    },
-                    lastNote: {
-                        type: "Biographical",
-                        date: "3/7/2016",
-                        summary: "Test Note"
-                    }
-                });
+                // FOR TESTING
+                matchedConstituent.id = 280;
+                foundMatch = true;
+                matchedConstituent.name = possibleConstituents[i].name;
+                
+                if (foundMatch)
+                {
+                    // Get basic constituent information https://api.sky.blackbaud.com/constituent/constituents/{constituentId}
+                    get("https://api.sky.blackbaud.com/constituent/constituents/" + matchedConstituent.id).then(function (data) {
+                        var obj = JSON.parse(data.results);
+                        // UNCOMMENT THIS WHEN ACTUAL RESPONSE RECEIVED
+                        //matchedConstituent.name = obj.name;
+                        matchedConstituent.constituentRecordUrl = "http://rexdev.blackbaud.com/constituents/" + matchedConstituent.id;
+                        matchedConstituent.phoneNumber = obj.phone.number;
+                        matchedConstituent.website = obj.online_presence.address;
+                        matchedConstituent.spouseName = obj.spouse.first + " " + obj.spouse.last;
+                        matchedConstituent.spouseRecordUrl = "http://rexdev.blackbaud.com/constituents/" + obj.spouse.id;
+                        
+                    })
+                    
+                    matchedConstituent.constituentCodes = [];
+                    // Get Constituent Codes https://api.sky.blackbaud.com/constituent/constituents/{constituentId}/constituentcodes
+                    get("https://api.sky.blackbaud.com/constituent/constituents/" + matchedConstituent.id + "/constituentcodes").then(function(data) {
+                        $.each(data.results, function(index, constitCode) {
+                            matchedConstituent.constituentCodes.push(constitCode.description);
+                        })
+                    })
+                    
+                    populateConstituentSidebar(matchedConstituent, threadView);
+                }
+                
+                else
+                {
+                    showSidebar(threadView, {
+                        name: possibleConstituents[i].name,
+                        constituentRecordUrl: "http://rexdev.blackbaud.com/constituents/280",
+                        phoneNumber: "843-234-1232",
+                        website: "http://blackbaud.com",
+                        spouseName: "Wendy Hernandez",
+                        spouseRecordUrl: "http://rexdev.blackbaud.com/constituents/410",
+                        constituentCodes: "Major Donor Prospect, Board Chair",
+                        lastAction: {
+                            type: "Meeting - Face to Face",
+                            date: "1/2/2015",
+                            summary: "Summary of action"
+                        },
+                        nextAction: {
+                            type: "Meeting - Face to Face",
+                            date: "6/2/2015",
+                            summary: "Summary of action"
+                        },
+                        lastNote: {
+                            type: "Biographical",
+                            date: "3/7/2016",
+                            summary: "Test Note"
+                        }
+                    });
+                }
+                
             }
         }
     });
